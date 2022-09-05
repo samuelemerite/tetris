d3a1b0da6ffa5af3f584886ad8b4e91d99621f4d
minkada ekani samuel emerite <samuel-e_m@qwasar.io>
2022-09-05 20:50:45 +0000

    ok

--- my_tetris.js
+++ my_tetris.js
@@ -1,15 +1,22 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'black';
+var color=["cyan","yellow","violet","green","red","blue","orange"];
 //ctx.fillRect(0, 0, 10, 10);
 pieces=[
+    [
+        [0,1,0,0],
+        [0,1,0,0],
+        [0,1,0,0],
+        [0,1,0,0]
+    ],
     [
         [1,1],
         [1,1]
     ],
     [
-        [0,0,0,0],
-        [1,1,1,1],
+        [1,1,1,0],
+        [0,1,0,0],
         [0,0,0,0],
         [0,0,0,0]
     ],
@@ -30,5 +37,38 @@ pieces=[
         [0,0,1,0],
         [0,0,1,0],
         [0,1,1,0]
+    ],
+    [
+        [0,0,0,0],
+        [0,0,1,0],
+        [0,0,1,0],
+        [0,0,1,1]
     ]
 ];
+
+const arene=[];
+const player={
+    pos:{ x:0, y: 0},
+}
+ctx.scale(4, 1);
+
+
+function drawPieces(pieces,x,y){
+
+}
+
+document.addEventListener('keydown', event => {
+    if(event.keyCode === 37 || event.keyCode === 52 || event.keyCode === 100 ){
+        player.pos.x--;
+    }else if( event.keyCode === 39 || event.keyCode ===54 || event.keyCode === 102 )
+    {
+        player.pos.x++;
+    }
+    else if (event.keyCode === 40 || event.keyCode === 104 || event.keyCode === 56)
+    {
+        player.pos.y++;
+        count =0;
+    }
+    /*else if (event.keyCode === 38 || event.keyCode === 50 || event.keyCode === 98 )
+    { player.matrix=rotation(player.matrix,1)}*/
+  });
