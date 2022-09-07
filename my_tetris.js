bd66cda432f4762c3ce4dc5d3ff0fa94bee905a2
minkada ekani samuel emerite <samuel-e_m@qwasar.io>
2022-09-07 01:25:15 +0000

    ok

--- my_tetris.js
+++ my_tetris.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("tetris");
 const ctx = canvas.getContext('2d');
-var color=["cyan","yellow","violet","green","red","blue","orange"];
+//var color=["cyan","yellow","violet","green","red","blue","orange"];
+ctx.scale(20, 20);
 //ctx.fillRect(0, 0, 10, 10);
 pieces=[
     [
@@ -42,33 +43,32 @@ pieces=[
         [0,0,1,0],
         [0,0,1,0],
         [0,0,1,1]
-    ]
+    ],
 ];
 
 const arene=[];
 const player={
-    pos:{ x:0, y: 0},
-    matrix: pieces[0]
-}
-ctx.scale(20, 20);
+    pos:{ x: 0, y: 0},
+    matrix: pieces[3] };
 
 
 function drawPieces(matrix,x,y){
 
-    for(let i=0 ; i<matrix.length ; i++)
+    for(let i=0 ; i < matrix.length ; i++)
     {
         for(let j=0; j<matrix[i].length; j++)
         {
             if(matrix[i][j])
             {
-                ctx.fillRect(color[2]);
-                ctx.fillRect(x+j,y+i,1,1);
+                ctx.fillStyle("red");
+                ctx.fillRect( x + j, y + i, 1, 1);
             }
         }
     }
 }
+drawPieces(player.matrix,player.pos.x,player.pos.y);
 
-document.addEventListener('keydown', event => {
+/*document.addEventListener('keydown', event => {
     if(event.keyCode === 37 || event.keyCode === 52 || event.keyCode === 100 ){
         player.pos.x--;
     }else if( event.keyCode === 39 || event.keyCode ===54 || event.keyCode === 102 )
@@ -81,6 +81,5 @@ document.addEventListener('keydown', event => {
         count =0;
     }
     /*else if (event.keyCode === 38 )
-    { player.matrix=rotation(player.matrix,1)}*/
-  });
-  drawPieces(player.matrix,player.pos.x,player.pos.y);
+    { player.matrix=rotation(player.matrix,1)}
+  });*/
