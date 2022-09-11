287145c3627a3072c67a8517bf6cf278b9a36269
minkada ekani samuel emerite <samuel-e_m@qwasar.io>
2022-09-11 14:12:11 +0000

    un peu d'evolution

--- my_tetris.js
+++ my_tetris.js
@@ -1,85 +1,85 @@
 const canvas = document.getElementById("tetris");
 const ctx = canvas.getContext('2d');
-//var color=["cyan","yellow","violet","green","red","blue","orange"];
-ctx.scale(20, 20);
-//ctx.fillRect(0, 0, 10, 10);
+ctx.scale(20,20);
+const colorPieces=["cyan","yellow","violet","green","red","blue","orange"];
 pieces=[
     [
-        [0,1,0,0],
-        [0,1,0,0],
-        [0,1,0,0],
-        [0,1,0,0]
+        [1,0,0,0],
+        [1,0,0,0],
+        [1,0,0,0],
+        [1,0,0,0]
     ],
     [
         [1,1],
         [1,1]
     ],
     [
-        [1,1,1,0],
-        [0,1,0,0],
-        [0,0,0,0],
-        [0,0,0,0]
+        [1,1,1],
+        [0,1,0],
+        [0,0,0]
     ],
     [
-        [0,0,1,1],
-        [0,1,1,0],
-        [0,0,0,0],
-        [0,0,0,0]
+        [0,1,1],
+        [1,1,0],
+        [0,0,0]
     ],
     [
-        [1,1,0,0],
-        [0,1,1,0],
-        [0,0,0,0],
-        [0,0,0,0]
+        [1,1,0],
+        [0,1,1],
+        [0,0,0]
     ],
     [
-        [0,0,0,0],
-        [0,0,1,0],
-        [0,0,1,0],
-        [0,1,1,0]
+        [0,1,0],
+        [0,1,0],
+        [1,1,0]
     ],
     [
-        [0,0,0,0],
-        [0,0,1,0],
-        [0,0,1,0],
-        [0,0,1,1]
-    ],
+        [0,1,0],
+        [0,1,0],
+        [0,1,1]
+    ]
 ];
+const arena = [];
 
-const arene=[];
-const player={
-    pos:{ x: 0, y: 0},
-    matrix: pieces[3] };
+const player ={
+    position: {x: 0, y: 0},
+    matrix:pieces[3]
+}
 
-
-function drawPieces(matrix,x,y){
-
-    for(let i=0 ; i < matrix.length ; i++)
+function drawPieces(matrix,x,y)
+{
+    for(let i=0;i<matrix.length;i++)
     {
-        for(let j=0; j<matrix[i].length; j++)
+        for(let j=0; j< matrix[i].length ; j++)
         {
             if(matrix[i][j])
-            {
-                ctx.fillStyle("red");
-                ctx.fillRect( x + j, y + i, 1, 1);
-            }
+                ctx.fillRect(x + j , y + i, 1, 1);
         }
     }
 }
-drawPieces(player.matrix,player.pos.x,player.pos.y);
 
-/*document.addEventListener('keydown', event => {
-    if(event.keyCode === 37 || event.keyCode === 52 || event.keyCode === 100 ){
-        player.pos.x--;
-    }else if( event.keyCode === 39 || event.keyCode ===54 || event.keyCode === 102 )
+let interval =1000;
+let lastTime = 0;
+let count = 0;
+
+function update(time = 0){
+
+    const dt = time -lastTime;
+    lastTime = time;
+    count += dt;
+
+    if(count >= interval)
     {
-        player.pos.x++;
+        player.position.y++;
+        count=0;
     }
-    else if (event.keyCode === 40)
-    {
-        player.pos.y++;
-        count =0;
-    }
-    /*else if (event.keyCode === 38 )
-    { player.matrix=rotation(player.matrix,1)}
-  });*/
+
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0 ,0 , canvas.Width, canvas.heigth);
+
+    ctx.fillStyle = colorPieces[1];
+    drawPieces(player.matrix , player.position.x , player.position.y);
+    requestAnimationFrame(update);
+}
+//drawPieces(player.matrix,player.position.x,player.position.y);
+update();
