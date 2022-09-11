52b5c1a57b00dd880a1752d8dafa921332acbe9c
minkada ekani samuel emerite <samuel-e_m@qwasar.io>
2022-09-11 14:42:10 +0000

    super

--- my_tetris.js
+++ my_tetris.js
@@ -48,10 +48,8 @@ const player ={
 
 function drawPieces(matrix,x,y)
 {
-    for(let i=0;i<matrix.length;i++)
-    {
-        for(let j=0; j< matrix[i].length ; j++)
-        {
+    for(let i=0;i<matrix.length;i++){
+        for(let j=0; j< matrix[i].length ; j++){
             if(matrix[i][j])
                 ctx.fillRect(x + j , y + i, 1, 1);
         }
@@ -62,24 +60,35 @@ let interval =1000;
 let lastTime = 0;
 let count = 0;
 
-function update(time = 0){
-
-    const dt = time -lastTime;
+function update( time = 0)
+{
+    const dt= time - lastTime;
     lastTime = time;
     count += dt;
-
-    if(count >= interval)
-    {
+    if( count >= interval ){
         player.position.y++;
         count=0;
     }
-
-    ctx.fillStyle = "#000";
-    ctx.fillRect(0 ,0 , canvas.Width, canvas.heigth);
-
-    ctx.fillStyle = colorPieces[1];
-    drawPieces(player.matrix , player.position.x , player.position.y);
+    ctx.fillStyle= '#fff';
+    ctx.fillRect(0, 0, canvas.clientWidth,canvas.clientHeight);
+    ctx.fillStyle = colorPieces[2];
+    drawPieces(player.matrix,player.position.x,player.position.y);
     requestAnimationFrame(update);
 }
+
+document.addEventListener("keydown", event =>{
+    if(event.keyCode === 37 || event.keyCode === 52 || event.keyCode === 100)
+    {
+        player.position.x--;
+    }
+    else if( event.keyCode ===39 || event.keyCode === 102 || event.keyCode === 54)
+    {
+        player.position.x++;
+    }
+    else if( event.keyCode === 40 || event.keyCode === 56 || event.keyCode === 104)
+    {
+        player.position.y++;
+    }
+})
 //drawPieces(player.matrix,player.position.x,player.position.y);
 update();
