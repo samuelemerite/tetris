6f834c77273b6bfd0f5cf7965801e4c70f158f45
minkada ekani samuel emerite <samuel-e_m@qwasar.io>
2022-09-13 12:47:48 +0000

    ok

--- my_tetris.js
+++ my_tetris.js
@@ -1,7 +1,12 @@
 const canvas = document.getElementById("tetris");
 const ctx = canvas.getContext('2d');
-ctx.scale(20,20);
+const scale= 20;
+ctx.scale(scale,scale);
 const colorPieces=["cyan","yellow","violet","green","red","blue","orange"];
+
+const twidth =canvas.clientWidth/ scale;
+const theight = canvas.clientHeight / scale ;
+
 pieces=[
     [
         [1,0,0,0],
@@ -56,6 +61,74 @@ function drawPieces(matrix,x,y)
     }
 }
 
+function rotation(matrix,dir)
+{
+    let newMatrix = [];
+
+    for (let i in matrix )
+    {
+        newMatrix.push([]);
+    }
+    
+    if(dir === 1)
+    {
+        for(let i=0; i< matrix.length; i++)
+        {
+            for(let j=0; j<matrix[i].length; j++)
+            {
+                newMatrix[j][matrix.length - i - 1] = matrix[i][j];
+            }
+        }
+    }
+    else {
+        for(let i=0; i< matrix.length; i++)
+        {
+            for(let j=0; j<matrix[i].length; j++)
+            {
+                newMatrix[matrix.length -j -1][i] = matrix[i][j];
+            }
+        }
+
+    }
+    return newMatrix;
+}
+
+/*function collides(player, arena)
+{
+    for(let i= 0; i< matrix.length ; i++)
+    {
+        for(let j=0 ; j< matrix[i].length; j++)
+        {
+            if( player.matrix[i][j] && arena[player.position.y + i +1][player.position.x + j +1])
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}*/
+
+/*function mergeArena( matrix , x, y)
+{
+    
+}*/
+
+function initArena()
+{
+    const r  = new Array(twidth +2).fill(1);
+    arena.push(r);
+    for(let i =0; i < theight; i++)
+    {
+        let row = new Array(twidth).fill(0);
+        row.push(1);
+        row.unshift(1);
+
+        arena.push(row);
+    }
+    arena.push(r);
+    arena.push(r);
+}
+
 let interval =1000;
 let lastTime = 0;
 let count = 0;
@@ -65,6 +138,13 @@ function update( time = 0)
     const dt= time - lastTime;
     lastTime = time;
     count += dt;
+    
+    /*if(collides(player,arena))
+    {
+        console.log('oof')
+    }*/
+
+
     if( count >= interval ){
         player.position.y++;
         count=0;
@@ -85,10 +165,14 @@ document.addEventListener("keydown", event =>{
     {
         player.position.x++;
     }
-    else if( event.keyCode === 40 || event.keyCode === 56 || event.keyCode === 104)
+    else if( event.keyCode === 40 || event.keyCode === 56 || event.keyCode === 98)
     {
         player.position.y++;
     }
+    else if ( event.keyCode === 38 || event.keyCode === 65 || event.keyCode === 104){
+        player.matrix =rotation(player.matrix,1);
+    }
 })
 //drawPieces(player.matrix,player.position.x,player.position.y);
+initArena();
 update();
